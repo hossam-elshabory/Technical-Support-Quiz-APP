bf70fdd7e37ffe802573553133c869fc82a68d2c
hossam el shabory <hossam.elshabory97@gmail.com>
2023-08-20 19:09:26 +0300

    Fix: added cairo time zone.

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.app_logo import add_logo
 from streamlit_extras.switch_page_button import switch_page
+import pytz  # Import the pytz library
 
 add_logo(
     "https://muwuzzuf.com/logos/l6zrcpVygq4D3LK8SzjzTrz855qmONM8N10NUSQR.png",
@@ -56,9 +57,15 @@ def main():
         color_name="violet-70",
     )
 
+    cairo_timezone = pytz.timezone("Africa/Cairo")  # Set Cairo timezone
+
     quiz_dates = {
-        "Second Quiz": datetime(2023, 8, 22),  # Replace with your quiz time
-        "Third Quiz": datetime(2023, 8, 31),  # Replace with your quiz time
+        "Second Quiz": cairo_timezone.localize(
+            datetime(2023, 8, 22)
+        ),  # Set Cairo timezone
+        "Third Quiz": cairo_timezone.localize(
+            datetime(2023, 8, 31)
+        ),  # Set Cairo timezone
     }
 
     card_style = "background-color: #333; padding: 15px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #555; border-radius: 5px; margin-bottom: 10px;"
@@ -67,7 +74,7 @@ def main():
     countdown_style = "color: #ff5733;"
 
     for quiz_name, quiz_date in quiz_dates.items():
-        time_difference = quiz_date - datetime.now()
+        time_difference = quiz_date - datetime.now(cairo_timezone)
         hours, minutes, seconds = format_time_difference(time_difference)
 
         countdown_div = f"<div id='{quiz_name}_countdown' style='{countdown_style}'>{hours:02}:{minutes:02}:{seconds:02}</div>"
@@ -76,7 +83,7 @@ def main():
         st.write(
             f"<div style='{card_style}'>"
             f"<strong style='{quiz_name_style}'>{quiz_name}:</strong> "
-            f"<u style='{time_style}'>{quiz_date.strftime('%B %d, %Y')}</u> - Countdown: {countdown_div}"
+            f"<u style='{time_style}'>{quiz_date.strftime('%B %d, %Y')}</u> - Countdown:{countdown_div}"
             f"</div>",
             unsafe_allow_html=True,
         )
