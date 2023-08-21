a617194ec115be970cdc8bebdbdc24414b964b39
hossam el shabory <hossam.elshabory97@gmail.com>
2023-08-22 02:52:47 +0300

    feat: Added question similariy search

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -13,15 +13,14 @@ add_logo(
 st.title("📲 WE TECH Training - Helper APP")
 st.markdown("---")
 
-st.markdown(
-    '<div style="background-color: #E8F4FD; padding: 10px; border-radius: 10px; border-left: 4px solid #3498DB; direction: rtl;">'
-    '<h3 style="color: #3498DB; margin-top: 0; width: 100%; text-align: right;">تحديثات</h3>'
-    '<ul style="list-style-type: disc; padding-inline-start: 30px; color: #333; font-family: Cairo; direction: rtl;">'
-    '<li><span style="font-size: 14px;">يتم الأن اضافة اسئله ال Quiz الثاني.</span></li>'
-    "</ul>"
-    "</div>",
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     '<div style="background-color: #E8F4FD; padding: 10px; border-radius: 10px; border-left: 4px solid #3498DB; direction: rtl;">'
+#     '<h3 style="color: #3498DB; margin-top: 0; width: 100%; text-align: right;">تحديثات</h3>'
+#     '<ul style="list-style-type: disc; padding-inline-start: 30px; color: #333; font-family: Cairo; direction: rtl;">'
+#     "</ul>"
+#     "</div>",
+#     unsafe_allow_html=True,
+# )
 
 st.write("")
 
@@ -39,53 +38,3 @@ with col3:
     sla_chat = st.button("💬 Find SLA of CASE ⌚")
     if sla_chat:
         switch_page("Sla_Chat")
-
-
-def format_time_difference(time_difference):
-    total_seconds = int(time_difference.total_seconds())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return hours, minutes, seconds
-
-
-def main():
-    colored_header(
-        label="⏳ Quiz Countdown",
-        description="",
-        color_name="violet-70",
-    )
-
-    cairo_timezone = pytz.timezone("Africa/Cairo")  # Set Cairo timezone
-
-    quiz_dates = {
-        "Second Quiz": cairo_timezone.localize(
-            datetime(2023, 8, 22)
-        ),  # Set Cairo timezone
-        "Third Quiz": cairo_timezone.localize(
-            datetime(2023, 8, 31)
-        ),  # Set Cairo timezone
-    }
-
-    card_style = "background-color: #333; padding: 15px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #555; border-radius: 5px; margin-bottom: 10px;"
-    quiz_name_style = "color: #00aaff;"
-    time_style = "color: #fff;"
-    countdown_style = "color: #ff5733;"
-
-    for quiz_name, quiz_date in quiz_dates.items():
-        time_difference = quiz_date - datetime.now(cairo_timezone)
-        hours, minutes, seconds = format_time_difference(time_difference)
-
-        countdown_div = f"<div id='{quiz_name}_countdown' style='{countdown_style}'>{hours:02}:{minutes:02}:{seconds:02}</div>"
-
-        # Using HTML to format the text and apply the card style
-        st.write(
-            f"<div style='{card_style}'>"
-            f"<strong style='{quiz_name_style}'>{quiz_name}:</strong> "
-            f"<u style='{time_style}'>{quiz_date.strftime('%B %d, %Y')}</u> - Countdown:{countdown_div}"
-            f"</div>",
-            unsafe_allow_html=True,
-        )
-
-
-if __name__ == "__main__":
-    main()
