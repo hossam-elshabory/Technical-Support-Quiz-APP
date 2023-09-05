87fb260092975ccb0dc34bc9e95febb902bfdc74
hossam el shabory <hossam.elshabory97@gmail.com>
2023-09-05 11:52:57 +0300

    Full update

---
+++ pages/5_📚_Matrial_PDFS.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from data import data
+
+st.title("📚 WE Training Material PDF")
+
+
+st.write("")
+
+st.info(
+    "💽 Google Drive Link: [Link](https://drive.google.com/drive/folders/1Ll06MG9uSDZzj1N5e23Z1BqAuHXpA7ZV?usp=drive_link)"
+)
